7e6be5d88cc93ff610f6f9856587829c297e9142
Ethan Huang <cherub0526@gmail.com>
2017-01-09 17:59:25 +0800

    更新 presentation.js

--- js/presentation.js
+++ js/presentation.js
@@ -115,33 +115,32 @@ var initEvent = function() {
     countdown();
 }
 
-var initData = function() {
+var initData = function(callback) {
     var options = {
         api: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
         // api: 'https://script.google.com/a/macros/csie.ntu.edu.tw/s/AKfycbx2VJc30mJ_EeQ2zN_8W0EYBoHf5yhyeuflgzx_bl6H/dev',
         sheet: 'team_list'
     };
 
+    // console.log(1);
+
     // 查詢資料
     queryData(options, function(response) {
 
         if (response.result) {
-            for(var i = 0; i < response.data.length; i++) {
-                // link.push(response.data[i].project_url);
-                link.push(response.data[i].slide_url);
-            }
-
-            $('#data-frame').attr('src', response.data[0].slide_url);
-
-            // 找出 team id 為 1 的資料
-            var team = $.grep(response.data, function(item) {
-                return item.team_id == 1;
-            });
-            $('#output').text(JSON.stringify(team, null, 4));
-
-            initEvent();
-        } else {
-            $('#output').text(JSON.stringify(response, null, 4));
+            callback(response.data);
         }
     });
 }
+
+initData(function(data) {
+    var link = [];
+
+    for(var i = 0; i < data.length; i++) {
+        link.push(data[i].slide_url);
+    }
+
+    $('#data-frame').attr('src', data[0].slide_url);
+
+    initEvent();
+});
