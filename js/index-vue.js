543b9ae230ddc3dbc3af54522a66cbd84550d08d
kchen-tw <kchen.ntu@gmail.com>
2017-01-10 02:28:25 +0800

    update index.html

--- js/index-vue.js
+++ js/index-vue.js
@@ -4,23 +4,26 @@ var team_card_group = new Vue({
         teams: [],
         apiUrl: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
     },
-    // beforeCreate: function() {
-    //     // var options = {
-    //     //     api: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
-    //     //     sheet: 'team_list',
-    //     // };
-    //     // queryData(options, function(res) {
-    //     //     console.log(res.data);
-    //     //     team_card_group.teams = res.data;
-    //     // });
-    //     this.getData();
-    // },
     beforeCreate: function() {
-        this.$http.jsonp(this.apiUrl, { sheet: 'team_list' })
-            .then(function(response) {
-                this.$set('teams', response.data);
-            });
+        // var options = {
+        //     api: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
+        //     sheet: 'team_list',
+        // };
+        // queryData(options, function(res) {
+        //     console.log(res.data);
+        //     team_card_group.teams = res.data;
+        // });
+        this.getData();
     },
+    // beforeCreate: function() {
+    //     this.$http.post(this.apiUrl, { sheet: 'team_list' })
+    //         .then(function(response) {
+    //             this.$set('teams', response.data);
+    //         })
+    //         .catch(function(response) {
+    //             console.log(response)
+    //         });
+    // },
     // ready: function() {
     //     // this.getData();
     //     console.log("ok");
