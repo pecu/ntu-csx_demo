c7d430bb85b5b0f63333b303e56ebc21452ae717
kchen-tw <kchen.ntu@gmail.com>
2017-01-10 02:31:09 +0800

    update index.html

--- js/index-vue.js
+++ js/index-vue.js
@@ -5,15 +5,15 @@ var team_card_group = new Vue({
         apiUrl: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
     },
     beforeCreate: function() {
-        // var options = {
-        //     api: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
-        //     sheet: 'team_list',
-        // };
-        // queryData(options, function(res) {
-        //     console.log(res.data);
-        //     team_card_group.teams = res.data;
-        // });
-        this.getData();
+        var options = {
+            api: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
+            sheet: 'team_list',
+        };
+        queryData(options, function(res) {
+            console.log(res.data);
+            team_card_group.teams = res.data;
+        });
+        // this.getData();
     },
     // beforeCreate: function() {
     //     this.$http.post(this.apiUrl, { sheet: 'team_list' })
