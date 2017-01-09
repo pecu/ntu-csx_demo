04e75aecf29f6ea5876229e6e09c3fd8e94ea5cb
kchen-tw <kchen.ntu@gmail.com>
2017-01-09 23:29:49 +0800

    add action:update in update.js

--- js/update.js
+++ js/update.js
@@ -5,7 +5,7 @@ $('#query').on('click', function() {
     var options = {
         //api: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
         api: 'https://script.google.com/a/macros/csie.ntu.edu.tw/s/AKfycbx2VJc30mJ_EeQ2zN_8W0EYBoHf5yhyeuflgzx_bl6H/dev',
-        // action: '',
+        action: 'update',
         sheet: 'team_score',
     };
 
