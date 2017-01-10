da27f0edc471f89460acf2b6dddce0288fe84864
kchen-tw <kchen.ntu@gmail.com>
2017-01-10 18:17:50 +0800

    update index.html demo

--- js/index-vue.js
+++ js/index-vue.js
@@ -2,12 +2,15 @@ var team_card_group = new Vue({
     el: '#team_card_group',
     data: {
         teams: [],
-        apiUrl: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
+        imgUrl: '',
     },
     beforeCreate: function() {
         var options = {
             api: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
             sheet: 'team_list',
+            filter: {
+                demo: true
+            }
         };
         queryData(options, function(res) {
             console.log(res.data);
