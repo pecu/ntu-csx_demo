1dc260f0c0ff5d7330a497e7b30ec476347260e7
kchen-tw <kchen.ntu@gmail.com>
2017-01-10 15:17:21 +0800

    update score.html

--- js/scoreSubmit.js
+++ js/scoreSubmit.js
@@ -1,27 +1,24 @@
 $('#query').on('click', function() {
-    if (formSubmit() == 0){
-        return ;
-    }
+    // if (formSubmit() == 0) {
+    //     return;
+    // }
     // 設定查詢資料
-    // dev api = https://script.google.com/a/macros/csie.ntu.edu.tw/s/AKfycbx2VJc30mJ_EeQ2zN_8W0EYBoHf5yhyeuflgzx_bl6H/dev
-    // exe api = https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec'
     var options = {
-        //api: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
-        api: 'https://script.google.com/a/macros/csie.ntu.edu.tw/s/AKfycbx2VJc30mJ_EeQ2zN_8W0EYBoHf5yhyeuflgzx_bl6H/dev',
-        action: 'updates',
+        api: 'https://script.google.com/macros/s/AKfycbzTfdt_q9aNqvWp7LW9JKy6sZeL9fK-KjDcsuaFdmoLlzYsu0-R/exec',
+        action: 'update',
         sheet: 'team_score',
     };
 
     var filter = {
-        team_id: $("input[name$='TeamID']")[0].value
+        team_id: parseInt($("input[name$='TeamID']")[0].value),
+        reviewer_id: parseInt($("input[name$='JudgeID']")[0].value),
     };
     var data = {
-        reviewer_id: $("input[name$='JudgeID']")[0].value,
-        score_1: $("input[name$='score1']")[0].value,
-        score_2: $("input[name$='score2']")[0].value,
-        score_3: $("input[name$='score3']")[0].value,
-        score_4: $("input[name$='score4']")[0].value,
-        score_5: $("input[name$='score5']")[0].value
+        score_1: parseFloat($("input[name$='score1']")[0].value),
+        score_2: parseFloat($("input[name$='score2']")[0].value),
+        score_3: parseFloat($("input[name$='score3']")[0].value),
+        score_4: parseFloat($("input[name$='score4']")[0].value),
+        score_5: parseFloat($("input[name$='score5']")[0].value),
     };
 
     //console.log(filter);
